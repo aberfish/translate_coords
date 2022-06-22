21c6f2f2e2a7fc87b6d866b225683b64a921efcb
Rose Awen Brindle <rab87@aber.ac.uk>
2022-06-22 12:13:32 +0100

    created camtodepth test file

---
+++ scripts/cam_to_depthcoords.py
@@ -0,0 +1,26 @@
+import rospy
+import ros_numpy
+from sensor_msgs.msg import PointCloud2
+
+import numpy as np
+
+def callback_ptcloud(ptcloud_data):
+    pc = ros_numpy.numpify(ptcloud_data)
+    points=np.zeros((pc.shape[0],3))
+    points[:,0]=pc['x']
+    points[:,1]=pc['y']
+    points[:,2]=pc['z']
+    print(pc)
+    quit()
+
+
+if __name__ == '__main__':
+    rospy.init_node("cam_to_depthcoords", anonymous=True)
+    rate = rospy.Rate(7) # ROS Rate at 5Hz
+    rospy.loginfo("Cam to Depth node started")
+    
+    rospy.Subscriber("/camera/depth/color/points", PointCloud2, callback_ptcloud)
+
+    while not rospy.is_shutdown():
+        rospy.spin()
+        rate.sleep()
