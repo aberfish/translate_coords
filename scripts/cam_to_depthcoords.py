e2ffe833b0876790dd33d6f4c14c096ad72bed25
Rose Awen Brindle <rab87@aber.ac.uk>
2022-07-04 09:52:37 +0100

    debugging camera start fail

--- scripts/cam_to_depthcoords.py
+++ scripts/cam_to_depthcoords.py
@@ -10,7 +10,7 @@ def callback_ptcloud(ptcloud_data):
     points[:,0] = pc['x']
     points[:,1] = pc['y']
     points[:,2] = pc['z']
-    rospy.loginfo(pc)
+    rospy.logerr(pc)
 
 
 if __name__ == '__main__':
