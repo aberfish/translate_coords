501e58e4a300d326cec97dfeed195a9e38fd36e3
Rose Awen Brindle <rab87@aber.ac.uk>
2022-07-05 10:18:30 +0100

    camcoord conv to camtodepth.py and add readme

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
 # translate_coords
+ROS package containing nodes for converting from 2d coordinates to 3d RTAB_map coordinates.
+
+- [translate_coords](#translate_coords)
+  - [Nodes](#nodes)
+    - [cam_to_depthcoords.py](#cam_to_depthcoordspy)
+      - [Subscribes:](#subscribes)
+      - [Publishes:](#publishes)
+      - [Parameters:](#parameters)
+      - [Usage:](#usage)
+    - [depth_to_mapcoords.py](#depth_to_mapcoordspy)
+      - [Subscribes:](#subscribes-1)
+      - [Publishes:](#publishes-1)
+      - [Parameters:](#parameters-1)
+      - [Usage:](#usage-1)
+  - [Setup](#setup)
+  - [Launch Files](#launch-files)
+
+## Nodes
+### cam_to_depthcoords.py
+Converts 2d pixel coordinates from depth camera colour images to realworld 3d coordinates measured in metres, where 0,0,0 is **WHERE**.
+
+#### Subscribes:
+ - */camera/depth/camera_info* (sensor_msgs.CameraInfo): Camera intrinsics data
+ - */camera/aligned_depth_to_color/image_raw* (sensor_msgs.Image): Depth image
+ - */input_coords* (geometry_msgs.Point): Input pixel location to be converted. 'z' is ignored
+
+#### Publishes:
+ - */realworld_coords* (geometry_msgs.Point): Position in metres, relative to the camera, of the input pixel location
+
+#### Parameters:
+*NONE*
+
+#### Usage:
+```rosrun translate_coords cam_to_depthcoords.py```
+
+### depth_to_mapcoords.py
+Converts 3d realworld coordinates measured in metres relative to the camera to RTAB_map coordinates.
+
+#### Subscribes:
+*NONE*
+ <!-- - */camera/depth/camera_info* (sensor_msgs.CameraInfo): Camera intrinsics data -->
+
+#### Publishes:
+*NONE*
+ <!-- - */realworld_coords* (geometry_msgs.Point): Position in metres, relative to the camera, of the input pixel location -->
+
+#### Parameters:
+*NONE*
+
+#### Usage:
+```rosrun translate_coords depth_to_mapcoords.py```
+
+## Setup
+Place contents of repository in the directory ```<ros_workspace_dir>/src/translate_coords```. Build and source your ROS workspace.
+
+## Launch Files
+<!-- ### tracker_debug
+Example usage: 
+
+```roslaunch tango_tracker tracker_debug.launch marker_size:=5 rosbag_path:="/home/rose/Documents/TangoProject/2022-06-06-14-05-29.bag"```
+
+#### Arguments
+ - *marker_size* (float): Measuerment in **centimeters** of aruco marker width/height
+ - *rosbag_path* (str): Absolute path to a rosbag containing the */camera/color/raw_image* topic -->
--- scripts/depth_to_mapcoords.py
+++ scripts/depth_to_mapcoords.py
@@ -1,23 +1,40 @@
 import rospy
 from sensor_msgs.msg import Image as msg_Image
+from geometry_msgs.msg import Point
 from sensor_msgs.msg import CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
+
 import pyrealsense2 as rs2
 
 intrinsics = None
+latest_depth_image = None
 bridge = CvBridge()
 
+def depthPointCallback(pnt):
+    global latest_depth_image, intrinsics
+
+    if intrinsics:
+        depth = pnt[2]
+
+        # convert depth from mm to metres
+        depth = depth/1000
+
+        # deproject to realworld coords in metres
+        result = rs2.rs2_deproject_pixel_to_point(intrinsics, [pnt[0], pnt[1]], depth)
+
+        # publish result
+        pnt_msg = Point(x=result[0], y=result[1], z=result[2])
+        depth_coords_realworld_pub.publish(pnt_msg)
+
+    else:
+        rospy.logwarn("Camera intrinsics not received yet")
+
 def imageDepthCallback(data):
-    global intrinsics
+    global latest_depth_image
+
     try:
         cv_image = bridge.imgmsg_to_cv2(data, data.encoding)
-        pix = (int(data.width/2), int(data.height/2)
-        print('Depth at center(%d, %d): %f(mm)\r' % (pix[0], pix[1], cv_image[int(pix[1]), pix[0]]))
-
-        if intrinsics:
-            depth = cv_image[pix[1], pix[0]]
-            result = rs2.rs2_deproject_pixel_to_point(intrinsics, [pix[0], pix[1]], depth)
-            print('result:', result)
+        latest_depth_image = cv_image
 
     except CvBridgeError as e:
         print(e)
@@ -41,6 +58,7 @@ def imageDepthInfoCallback(cameraInfo):
         elif cameraInfo.distortion_model == 'equidistant':
             intrinsics.model = rs2.distortion.kannala_brandt4
         intrinsics.coeffs = [i for i in cameraInfo.D]
+
     except CvBridgeError as e:
         print(e)
         return
@@ -49,7 +67,10 @@ def imageDepthInfoCallback(cameraInfo):
 if __name__ == '__main__':
     rospy.init_node("depth_to_mapcoords")
 
-    sub = rospy.Subscriber("/camera/depth/image_rect_raw", msg_Image, imageDepthCallback)
-    sub_info = rospy.Subscriber('/camera/depth/camera_info', CameraInfo, imageDepthInfoCallback)
+    depth_img_sub = rospy.Subscriber("/camera/depth/image_rect_raw", msg_Image, imageDepthCallback)
+    cam_info_sub = rospy.Subscriber('/camera/depth/camera_info', CameraInfo, imageDepthInfoCallback)
+    input_coords_sub = rospy.Subscriber('pc_coords', Point, depthPointCallback)
+
+    depth_coords_realworld_pub = rospy.Publisher('realworld_depth_coords', Point, queue_size=10)
 
     rospy.spin()
